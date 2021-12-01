7c7c2b8f6d8ea58a5765432badd2ae230ad6d427
Félix Ravella <felix.ravella@gmail.com>
2021-12-01 15:47:44 +0100

    nettoyage quotes brouillon

---
+++ day_01/part01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 11:13:00 2021
+
+@author: felix
+"""
+
+total_upward = 0;
+previous = None
+
+for current in open("./01_input.txt").readlines():
+  current = int(current)
+
+  if previous:
+    total_upward += 1 if previous < current else 0
+    
+  previous = current
+
+print(total_upward)
