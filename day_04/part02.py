099ff666b266dfd19e7b7405ea89b1d5097fe202
Félix Ravella <felix.ravella@gmail.com>
2021-12-04 23:13:09 +0100

    day 04

---
+++ day_04/part02.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  4 22:53:30 2021
+
+@author: felix
+"""
+class Cell:
+  def __init__(self, value):
+    self.value = int(value)
+    self.marked = False
+
+class Board:
+  def __init__(self, rows):
+    self.rows = []
+    self.unmarked_sum = 0
+    self.winner = False
+    for row in rows:
+      new_row = []
+      for i in range(5):
+        value = int(row[i])
+        new_row.append(Cell(value))
+        self.unmarked_sum += value
+      self.rows.append(new_row)
+        
+  def mark_and_check_winner(self, number):
+    for row in self.rows:
+      for cell in row:
+        if cell.value == number:
+          cell.marked = True
+          self.unmarked_sum -= number
+          if self.is_winner():
+            return(number)
+    return(-1)
+  
+  def is_winner(self):
+    for row in self.rows:
+      marked_number = 0
+      for cell in row:
+        marked_number += int(cell.marked)
+      if marked_number == 5:
+        return(True)
+    
+    for col_number in range(5):
+      marked_number = 0
+      for row in self.rows:
+        marked_number += int(row[col_number].marked)
+      if marked_number == 5:
+        return(True)
+
+
+lines = open("./input.txt").readlines()
+
+boards = []
+numbers = lines.pop(0).replace('\n', '').split(',')
+winners = 0
+
+while len(lines) > 5:
+  lines.pop(0)
+  rows = []
+  for i in range(5):
+    rows.append(' '.join(lines.pop(0).split()).split(' '))
+    
+  boards.append(Board(rows))
+
+
+def get_last_winner():
+  global winners
+  for num in numbers:
+    num = int(num)
+    for board in boards:
+      if not board.winner == True:
+        if board.mark_and_check_winner(num) > -1:
+          board.winner = True
+          winners += 1
+          if winners == len(boards):
+            return(board.unmarked_sum * num)
+          
+
+print(get_last_winner()) 
+ 
+  
